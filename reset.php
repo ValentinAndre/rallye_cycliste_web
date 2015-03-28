41b41c3930487b52949e7b300a3968a03a88a719
durotm <durotm@localhost.localdomain>
2015-03-28 16:55:14 +0100

    Ajout reset.php MAJ TODO list

---
+++ reset.php
@@ -0,0 +1,16 @@
+<?php
+require_once ('php/session.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+		<?php require_once('php/head.php'); ?>
+		<script src="js/pointage.js"></script>
+<title>Pointage</title>
+</head>
+<body>
+		<?php require_once('php/menu.php'); ?>
+		<div class="container"></div>
+</body>
+</html>
