a49e8fbdcf25fc96bb020222f22cb3df77af2369
durotm <durotm@localhost.localdomain>
2015-03-28 19:35:13 +0100

    Ajout d'une classe pour la gestion des parcours

---
+++ js/classes/TableauParcours.js
@@ -0,0 +1,49 @@
+/*
+ * Objet représentant un tableau de parcours
+ */
+
+function TableauParcours() {
+
+		// Constructeur //
+	
+	var data = [];
+	
+	var postData = {
+		action : 'retrieveAll',
+		'class' : 'Parcours',
+		dao : 'ParcoursDAO'
+	}
+
+	$.post("php/crud.php", postData, function(liste) {
+		for (i in liste) {			
+			data[i] = liste[i];
+		}
+	}, 'json');
+
+	this.parcours = data;
+	
+		// Méthodes //
+	
+	/*
+	 * Donne le nom d'un parcours à partir d'un indice
+	 * @param : un entier
+	 * @return : une chaine
+	 */
+	this.getName = function(indice) {
+		if (indice >= 0 && indice < this.parcours.length)
+			return this.parcours[indice].type + this.parcours[indice].distance;
+		else
+			return -1;
+	}
+	
+	/*
+	 * Remplit un select de parcours
+	 * @param : un select (objet DOM)
+	 */
+	this.feedSelect = function(select) {
+		for (i=0; i<this.parcours.length; i++)
+			select.appendChild(
+					new Option(this.parcours[i].type + this.parcours[i].distance,
+							this.parcours[i].idParcours));	
+	}
+}
