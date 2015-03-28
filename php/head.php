1d1c1aa4bc1a507bb00b67ee71d53b2b72323d08
durotm <durotm@localhost.localdomain>
2015-03-28 22:16:13 +0100

    Rennomages divers

--- php/head.php
+++ php/head.php
@@ -2,8 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
 <link href="css/main.css" rel="stylesheet"/>
-<script src="js/jquery.min.js"></script>
+<script src="js/libraries/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
-<script src="js/chart.js"></script>
-<script src="js/tablesorter.js"></script>
+<script src="js/libraries/chart.min.js"></script>
+<script src="js/libraries/tablesorter.min.js"></script>
 <script src="js/classes/TableauParcours.js"></script>
