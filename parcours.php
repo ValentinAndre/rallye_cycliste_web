7f95ed92fda329adb0b36762fa0e706eba288a19
durotm <durotm@localhost.localdomain>
2015-03-30 23:10:28 +0200

    Fin module de gestion des parcours

--- parcours.php
+++ parcours.php
@@ -1,4 +1,5 @@
 <?php
+function __autoload($class) { require_once "php/classes/$class.php"; };
 require_once ('php/session.php');
 
 // On vérifie qu'il s'agit bien de l'admin
@@ -6,43 +7,98 @@ if ($_SESSION ['login'] !== 'admin') {
 	header ( "Location: login.php" );
 	exit ( 0 );
 }
+
+// Initialisation des erreurs
+$_POST['erreur'] = false;
+
+$parcours = new ParcoursDAO(MaBD::getInstance());
+$inscriptions = new InscriptionsDAO(MaBD::getInstance());
+$new = new Parcours(array('idParcours' => DAO::UNKNOWN_ID,
+									'type' => "",
+									'distance' => ""));
+
+function afficherFormulaire() {
+	global $parcours, $new;			
+	
+	foreach ($parcours->getAll() as $p)
+		$p->toForm();
+	
+	$new->toForm();
+}
+
+// Affiche un message d'erreur ou d'information
+function afficherMessage() {
+	if (isset($_POST['message']) && isset($_POST['erreur'])) {
+		if ($_POST['erreur'])
+			echo '<div id="erreur" class="alert alert-danger center-text" role="alert">',
+				  $_POST['message'], '</div>';
+		else
+			echo '<div id="info" class="alert alert-info center-text" role="alert">',
+				  $_POST['message'], '</div>';
+	}
+}
+
+// Ajout d'un parcours
+if (isset($_POST['add'])) {	
+	$p = new Parcours(array('idParcours' => $_POST['id'],
+									'type' => $_POST['type'], 
+									'distance' => $_POST['distance']));
+	$parcours->insert($p);
+	$_POST['message'] = "Le parcours a bien été ajouté !";
+}
+
+
+// Suppression d'un parcours
+if (isset($_POST['supp'])) {
+	if (!$inscriptions->checkRef($_POST['id'])) {
+		$p = $parcours->getOne($_POST['id']);
+		$parcours->delete($p);
+		$_POST['message'] = "Le parcours a bien été supprimé !";
+	} else {
+		$_POST['erreur'] = true;
+		$_POST['message'] = "Impossible de supprimer le parcours,
+							 il fait référence à une ou plusieurs inscriptions !";
+	}
+}
+
+// Modification d'un parcours
+if (isset($_POST['modif'])) {
+	if (!$inscriptions->checkRef($_POST['id'])) {
+		$p = new Parcours(array('idParcours' => $_POST['id'],
+				'type' => $_POST['type'],
+				'distance' => $_POST['distance']));
+		$parcours->update($p);
+		$_POST['message'] = "Le parcours a bien été mis à jour";
+	} else {
+		$_POST['erreur'] = true;
+		$_POST['message'] = "Impossible de modifier le parcours,
+							 il fait référence à une ou plusieurs inscriptions !";
+	}	
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-		<?php require_once('php/head.php'); ?>
-		<script src="js/parcours.js"></script>
+		<?php require_once('php/head.php'); ?>		
 <title>Parcours</title>
 </head>
 <body>
 		<?php require_once('php/menu.php'); ?>
 		<div class="container">
-		<div class="row">
-			<div class="page-header center-text">
-				<h1>Gestion des parcours</h1>
+			<div class="row">
+				<div class="page-header center-text">
+					<h1>Gestion des parcours</h1>
+				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<th><span class="glyphicon glyphicon-asterisk">&nbsp;</span>Identifiant</th>
-							<th><span class="glyphicon glyphicon-resize-vertical">&nbsp;</span>Distance</th>
-							<th><span class="glyphicon glyphicon-tags">&nbsp;</span>Type</th>
-						</tr>
-					</thead>
-					<tbody>
-					</tbody>
-					<tfoot>
-						<td></td>
-						<td></td>
-						<td></td>
-					</tfoot>
-				</table>
+			<div class="row">
+				<div class="col-lg-offset-4 col-lg-4 col-lg-offset-4">
+					<?php afficherFormulaire();?>
+					<div class="vertical-offset-20">
+						<?php afficherMessage();?>
+					</div>				
+				</div>				
 			</div>
-		</div>
 	</div>
 </body>
 </html>
--- php/classes/Parcours.php
+++ php/classes/Parcours.php
@@ -8,12 +8,32 @@ class Parcours extends TableObject {
          return "$this->idParcours, $this->distance, $this->type";
     }    
     
-    public function toTableRow(){
-    	echo '<tr><td>', $this->idParcours, '</td><td>', $this->distance, '</td><td>', $this->type, '</td></tr>';
-    }
-    
-    public function getFields() {
-    	return $this->getAllFields();
+    public function toForm() {
+    	$selected = ($this->type == "ROUTE") ? "selected" : "";
+    	
+    	echo '<form class="form-inline vertical-offset-5 center-text" action="', $_SERVER['PHP_SELF'], '" method="post">
+              		<div class="form-group">
+    					<input type="hidden" value="', $this->idParcours, '" name="id">
+    					<select class="form-control input-sm" name="type">
+    						<option value="VTT">VTT</option>
+   							<option value="ROUTE" ', $selected, '>ROUTE</option>
+   						</select>
+   						<input class="form-control center-text input-sm" type="text" value="',
+   								$this->distance, '" name="distance" placeholder="Distance..." required>';
+    	
+    	if ($this->idParcours === DAO::UNKNOWN_ID)
+    		echo '<button type="submit" class="icon-button" name="add[]">
+				  		<span class="glyphicon glyphicon-plus" aria-hidden="true"> </span>
+				  </button>';
+    	else
+    		echo '<button type="submit" class="icon-button" name="modif[]">
+				  		<span class="glyphicon glyphicon-pencil" aria-hidden="true"> </span>
+				  </button>
+    			  <button type="submit" class="icon-button" name="supp[]">
+				  		<span class="glyphicon glyphicon-remove" aria-hidden="true"> </span>
+				  </button>';
+    	
+    	echo '</div></form>';
     }
 }
 ?>
