e4677dfd510fbf7edf978fcc4cbbb303833e99b4
durotm <durotm@localhost.localdomain>
2015-03-31 16:42:21 +0200

    MAJ Classe

--- php/classes/Club.php
+++ php/classes/Club.php
@@ -4,9 +4,14 @@ class Club extends TableObject {
 	static public $keyFieldsNames = array('clubOuVille'); // par défaut un seul champ
 	public $hasAutoIncrementedKey = false;
 	
+	public $fields;
+	
+	public function __construct($fields) {
+		$this->fields = $fields;
+	}
 	public function toTableRow() {
 		echo "<tr>";
-		foreach($this->getFieldsValuesWithoutKey() as $value)
+		foreach($this->fields as $key => $value)
 			echo "<td>", $value, "</td>";
 		echo "</tr>\n";
 	}
