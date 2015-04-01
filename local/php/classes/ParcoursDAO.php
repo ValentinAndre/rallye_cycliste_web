f0ae940d1450d665fad7f70196e297139b720b82
Valou <Valou@192.168.1.65>
2015-04-01 10:13:00 +0200

    fork du projet pour partie web

--- local/php/classes/ParcoursDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Classe pour l'accès à la table PARCOURS
-class ParcoursDAO extends DAO
-{
-    protected $table = "PARCOURS";
-    protected $class = "Parcours";
-}
-?>
--- local/php/tests/testStatistiques.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Autochargement des classes
-function __autoload($class) { require_once "../classes/$class.php"; }
-
-$stats = new Statistiques(MaBD::getInstance());
-echo $stats->getNumberOfInscriptions();
-echo $stats->getYoungest("h");
-?>
